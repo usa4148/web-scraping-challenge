e28f8890a4e0711368d837ea62308c58e2bef97c
Dan Cusick <dan.cusick@gmail.com>
2021-05-10 00:06:09 -0500

    Working on mongo in the flask app.

--- Missions_to_Mars/flask_app.py
+++ Missions_to_Mars/flask_app.py
@@ -5,7 +5,7 @@ import scrape_mars.py
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
 
 # Or set inline
@@ -14,15 +14,15 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    surface = mongo.db.surface.find_one()
+    return render_template("index.html", surface=surface)
 
 
 @app.route("/scrape")
 def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
+    surface = mongo.db.mars_db
+    surface_data = scrape_mars.scrape()
+    surface.update({}, surface_data, upsert=True)
     return redirect("/", code=302)
 
 
