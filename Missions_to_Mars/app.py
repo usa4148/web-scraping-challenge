5bd481b73d15459e509a49adff23565001d626f4
Dan Cusick <dan.cusick@gmail.com>
2021-05-10 23:07:23 -0500

    Renamed flask_app to app

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from scrape_mars import scrape
+#import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of dat from the mongo database
+    surface_data = mongo.db.surface.find_one()
+
+    # Return template and data
+    return render_template("index.html", surface=surface_data)
+
+
+@app.route("/scrape")
+def scraper():
+    
+    # Run the scrape function
+    surface_data = scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.surface.update({}, surface_data, upsert=True)
+    
+    return redirect("/")
+    #return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -9,12 +9,13 @@ from bs4 import BeautifulSoup
 
 # Import Splinter and set the chromedriver path
 from splinter import Browser
-
+import pandas as pd
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
 
+    # Set up Splinter
     executable_path = {"executable_path": "chromedriver"}
     browser = Browser("chrome", **executable_path, headless=False)
 
@@ -46,10 +47,17 @@ def scrape():
     browser.quit()
     
     bigbaliwick = {
-        "mars_img": img_url,
-        "mars_comparison": tables[0],
-        "mars_specs": tables[1]
+        "mars_img": img_url
+    #    "mars_comparison": tables[0],
+    #    "mars_specs": tables[1]
     }
+    #    bigbaliwick = {
+    #    "mars_img": img_url,
+    #    "mars_comparison": tables[0],
+    #    "mars_specs": tables[1]
+    #}
+
+    browser.quit()
     
     # Return Results
     return bigbaliwick
