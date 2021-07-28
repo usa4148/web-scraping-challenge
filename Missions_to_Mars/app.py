4377bbf7441c65146aec3f442fd68920e0bc6971
Dan Cusick <dan.cusick@gmail.com>
2021-07-28 12:29:13 -0500

    Sorting the old code, checking connectivity, scraping still looks to be working.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-#from selenium import webdriver
-from scrape_mars import scrape_info
-#import scrape_mars
+#from scrape_mars import scrape_info
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
